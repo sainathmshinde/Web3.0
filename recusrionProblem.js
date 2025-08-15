7b9be3eeb3c32c157e4c88699228f23f3c9c9102
Sainath Shinde <shindesainath692@gmail.com>
2025-08-15 22:01:34 +0530

    Recursion add even numbers

--- recusrionProblem.js
+++ recusrionProblem.js
@@ -20,3 +20,17 @@ function fun(n) {
   fun(--n);
 }
 fun(10);
+
+/// add even numbers
+
+let arr = [5, 4, 3, 2, 0, 1];
+function sum(n) {
+  if (n == 0) return arr[0] % 2 === 0 ? arr[0] : 0;
+  if (arr[n] % 2 == 0) {
+    return arr[n] + sum(n - 1);
+  } else {
+    return sum(n - 1);
+  }
+}
+
+console.log(sum(arr.length - 1));
