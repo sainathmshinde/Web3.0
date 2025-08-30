fc5c4cabcb54a9d68846e7db9e94fe4f51a40c3f
Sainath Shinde <shindesainath692@gmail.com>
2025-08-30 23:05:54 +0530

    Selection sort

---
+++ selectionSort.js
@@ -0,0 +1,27 @@
+// sort by finding minimum num inaarray
+
+let arr = [7, 1, 5, 2, 3, 6, 4];
+
+function selectionSort(arr) {
+  let n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    let min = i;
+
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[min]) {
+        min = arr[j];
+      }
+    }
+    if (min != i) {
+      let temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp;
+    }
+  }
+  return arr;
+}
+
+let result = selectionSort(arr);
+
+console.log(result);
